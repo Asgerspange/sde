834a713c6a62bf47e6301bd3d3bd3c060f1b0b5c
Asger Spange <asgerspange@gmail.com>
2024-02-05 09:13:17 +0100

    setup solution

---
+++ Grundlæggende prog/Opgaver/Projekt1/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+ 
+namespace Projekt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, World!");
+        }
+    }
+}
